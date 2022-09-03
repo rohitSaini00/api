4528565239b056a640a7b6a80a7d1975ac032d4d
rohitSaini <rohitkumarweb934@gmail.com>
2022-09-03 09:55:46 +0530

    state wise data fetach complete

--- src/userEffectApi/UseEffectApi.js
+++ src/userEffectApi/UseEffectApi.js
@@ -10,6 +10,7 @@ const UseEffectApi = () => {
   };
   const [data, setData] = useState([]);
   const [value, setValue] = useState(getInitialState);
+  console.log(data);
   const mydata = data[value];
   console.log(mydata);
 
@@ -36,14 +37,42 @@ const UseEffectApi = () => {
           value={value}
           onChange={onChange}
         >
+          <option selected>Selet State</option>
           <option value="AP">Andra Pradesh</option>
-          <option value="UP">Uttar Pradesh</option>
+          <option value="AR">Arunachal Pradesh</option>
+          <option value="BR">Bihar</option>
+          <option value="CH">Chandigarh</option>
+          <option value="CH">Chhattisgarh</option>
+          <option value="DL">Delhi</option>
+          <option value="GA">Goa</option>
+          <option value="JH">Jharkhand</option>
+          <option value="JK">Jammu and Kashmir</option>
+          <option value="HP">Himachal Pradesh</option>
+          <option value="HR">Haryana</option>
+          <option value="KR">Karnataka</option>
+          <option value="KA">Kerla</option>
+          <option value="LA">Lakshyadeep</option>
+          <option value="MH">Maharatha</option>
+          <option value="ML">Meghalaya</option>
+          <option value="MN">Manipura</option>
           <option value="MP">Mandhya Pradesh</option>
+          <option value="MZ">Mizoram</option>
+          <option value="NL">Nagaland</option>
+          <option value="OR">Odisha</option>
+          <option value="PB">Punjab</option>
+          <option value="PY">Pandhyacheri</option>
+          <option value="RJ">Rajasthan</option>
+          <option value="SK">Sikkim</option>
+          <option value="TR">Tripura</option>
+          <option value="TN">Tamil Nadu</option>
+          <option value="UP">Uttar Pradesh</option>
+          <option value="GJ">Gujrat</option>
+          <option value="WB">West Bengal</option>
         </select>
       </div>
       <div className="container">
         <div className="box">
-          <table border="2px solid red" class="mytable">
+          <table border="2px solid red" className="mytable">
             <thead>
               <th>S.No</th>
               <th>Title</th>
@@ -53,37 +82,47 @@ const UseEffectApi = () => {
               <tr>
                 <th>1</th>
                 <th scope="row">Confirmed Cases</th>
-                <td>{mydata?.delta?.confirmed}</td>
+                <td>
+                  {mydata?.delta?.confirmed ? mydata?.delta?.confirmed : 0}
+                </td>
               </tr>
               <tr>
                 <th>2</th>
                 <th scope="row">Deceased Cases</th>
-                <td>{mydata?.delta?.deceased}</td>
+                <td>{mydata?.delta?.deceased ? mydata?.delta?.deceased : 0}</td>
               </tr>
               <tr>
                 <th>3</th>
                 <th scope="row">Recovered Cases</th>
-                <td>{mydata?.delta?.recovered}</td>
+                <td>
+                  {mydata?.delta?.recovered ? mydata?.delta?.recovered : 0}
+                </td>
               </tr>
               <tr>
                 <th>4</th>
                 <th scope="row">Tested</th>
-                <td>{mydata?.delta?.tested}</td>
+                <td>{mydata?.delta?.tested ? mydata?.delta?.tested : 0}</td>
               </tr>
               <tr>
                 <th>5</th>
                 <th scope="row">Vaccinated1</th>
-                <td>{mydata?.delta?.vaccinated1}</td>
+                <td>
+                  {mydata?.delta?.vaccinated1 ? mydata?.delta?.vaccinated1 : 0}
+                </td>
               </tr>
               <tr>
                 <th>6</th>
                 <th scope="row">Vaccinated2</th>
-                <td>{mydata?.delta?.vaccinated2}</td>
+                <td>
+                  {mydata?.delta?.vaccinated2 ? mydata?.delta?.vaccinated2 : 0}
+                </td>
               </tr>
               <tr>
                 <th>7</th>
                 <th scope="row">Last Updated</th>
-                <td>{mydata?.meta?.last_updated}</td>
+                <td>
+                  {mydata?.meta?.last_updated ? mydata?.meta?.last_updated : 0}
+                </td>
               </tr>
             </tbody>
           </table>
